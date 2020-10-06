f9e5109f9c21a6c1245718e3be17826bf281bf4a
Albert <countrik@yandex.ru>
2020-10-06 18:05:44 +0300

    update

--- README.md
+++ README.md
@@ -4,12 +4,20 @@
 
 Добавим в */etc/sesuruty/time.conf* строчку ``login;*;!root;Wd0000-2400``, которая запрещает логин в выходные, кроме пользователя root.
 
-Добавим в */etc/pam.d/login* после строчки *account required pam_nologin.so* еще две строчки
+Добавим в */etc/pam.d/login* после строчки *account required pam_nologin.so* еще две строчки:
+
+Эта строчка указывает тип*account* (контроль ресурсов), *[default=ignore success=1]* флаг контроля где при успешном срабатывании модуля происходит переход через одно правило (success=1), в остальных срабавтываниях игнорируется (default=ignore), *pam_succeed_if.so  user ingroup admin* условие срабатывания, pam_succeed_if.so - успешное срабатывание если - user ingroup admin - пользователь состоит в группе admin.
 
 ``
 account [default=ignore success=1] pam_succeed_if.so  user ingroup admin
 ``
 
+
+Эта строчка, которая идет сразу после предыдущей, ограничивает пользователей по времени, параметры которой задаются в файле /etc/security/time.conf
+
 ``
 account    required     pam_time.so
 ``
+
+
+В папке *scripts* приведен пример скрипта с комментариями.
--- scripts/pam.sh
+++ scripts/pam.sh
@@ -16,16 +16,16 @@ useradd neadmin
 echo "admin2" | sudo passwd --stdin admin2
 echo "neadmin" | sudo passwd --stdin neadmin
 
-# Добавим параметры ограничения по времени в /etc/security/time.conf
-# Здесь указан пользователь vagrant, чтобы можно было проверить на стенде vagrant, в других случаях можно заменить на пользователя root
-echo "*;*;!vagrant;Wd0000-2400"
+# Добавим параметры ограничения по времени (запретим всем пользователям, кроме группы admin логин в выходные (суббота и воскресенье)) в /etc/security/time.conf
+# Здесь указан пользователь vagrant, чтобы можно было проверить на стенде vagrant, в других случаях можно заменить например на пользователя root
+echo '*;*;!vagrant;!Wd0000-2400' >> /etc/security/time.conf
 
-# Добавим параметры в /etc/pam.d/login
-# Также добавим для наглядности и проверки на стенде vagrant этиже параметры в /etc/pam.d/sshd
-sed '7a\account    required     pam_time.so' /etc/pam.d/login 
-sed '7a\account [default=ignore success=1] pam_succeed_if.so  user ingroup admin' 
+# Добавим строки параметров в /etc/pam.d/login
+sed -i '5a\account    required     pam_time.so' /etc/pam.d/login
+sed -i '5a\account [default=ignore success=1] pam_succeed_if.so  user ingroup admin' /etc/pam.d/login
 
-
-
+# Также для наглядности и проверки на стенде vagrant добавим эти же строки параметров в /etc/pam.d/sshd
+sed -i '7a\account    required     pam_time.so' /etc/pam.d/sshd
+sed -i '7a\account [default=ignore success=1] pam_succeed_if.so  user ingroup admin' /etc/pam.d/sshd
 # Разрешим вход через ssh по паролю
 sudo bash -c "sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service"
